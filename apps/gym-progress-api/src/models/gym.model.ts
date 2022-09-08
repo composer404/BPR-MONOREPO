e8038a5faef71649c3678e7d1e3f50020b8605b5
Kozik Przemysław <Przemyslaw.Kozik@assecods.pl>
2022-09-08 15:13:19 +0200

    Added functions to gym managing

--- apps/gym-progress-api/src/models/gym.model.ts
+++ apps/gym-progress-api/src/models/gym.model.ts
@@ -1,8 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+/* ------------------------------- INTERFACES ------------------------------- */
 export interface GymInput {
     id?: string;
     name: string;
     street: string;
     post_code: string;
     country: string;
+    street_number: string;
+}
+
+export interface AdministratorInput {
+    id?: string;
+    login: string;
+    password: string;
+    information: string;
+}
+
+/* --------------------------------- CLASSES -------------------------------- */
+export class AdministratorInputClass {
+    @ApiProperty()
+    login: string;
+    @ApiProperty()
+    password: string;
+    @ApiProperty()
+    information: string;
+}
+
+export class GymInputClass {
+    @ApiProperty()
+    name: string;
+    @ApiProperty()
+    street: string;
+    @ApiProperty()
+    post_code: string;
+    @ApiProperty()
+    country: string;
+    @ApiProperty()
     street_number: string;
 }
--- apps/gym-progress-api/src/modules/gyms/gyms.controller.ts
+++ apps/gym-progress-api/src/modules/gyms/gyms.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 import { ApiTags } from '@nestjs/swagger';
 import { Gym } from '@prisma/client';
+import { AdministratorInputClass, CreatedObjectResponse, GymInputClass } from 'src/models';
 
 import { GymsService } from './gyms.service';
 
@@ -18,5 +19,20 @@ export class GymsController {
     @Get(':name')
     async findGymByName(@Param() params: any): Promise<Gym | null> {
         return this.gymsService.findGymById(params.name);
+    }
+
+    @Post(':id/administrators')
+    async addAdministratorToGym(@Param() params: any, @Body() body: AdministratorInputClass) {
+        return this.gymsService.addAdministratorToGym(params.id, body);
+    }
+
+    @Post()
+    async createGym(@Body() input: GymInputClass): Promise<CreatedObjectResponse | null> {
+        return this.gymsService.createGym(input);
+    }
+
+    @Put(':id')
+    async updateGym(@Param() params: any, @Body() input: GymInputClass): Promise<boolean> {
+        return this.gymsService.updateGym(params.id, input);
     }
 }
