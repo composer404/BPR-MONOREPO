4557bdbc93511970bbadb2bdb991b2bff5ea506c
Kozik Przemysław <Przemyslaw.Kozik@assecods.pl>
2022-09-09 19:50:26 +0200

    Resolved problems with imports and dependencies

--- apps/ionic-gym-progress-app/src/app/app.module.ts
+++ apps/ionic-gym-progress-app/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 import { NgModule } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+    providers: [MessageService, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
