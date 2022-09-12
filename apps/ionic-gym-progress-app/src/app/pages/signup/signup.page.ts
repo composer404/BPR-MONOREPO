11089a2af2782898067378736c59d771c1acdfd2
Ioana Estera Dumitrache <ide@novicell.dk>
2022-09-12 15:38:29 +0200

    Signup but with cors problem

--- apps/ionic-gym-progress-app/src/app/pages/signup/signup.page.ts
+++ apps/ionic-gym-progress-app/src/app/pages/signup/signup.page.ts
@@ -1,6 +1,6 @@
 import {API_ERROR_CODES, BPR_ERROR_CODES} from '../../interfaces/interfaces';
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 import {AuthService} from '../../services/auth/auth.service';
 import {InfoService} from '../../services/api/info.service';
@@ -14,19 +14,20 @@ import {Router} from '@angular/router';
 export class SignupPage implements OnInit {
    signupForm: FormGroup;
    avatar: any;
+   unauthorized: boolean;
  
   constructor(private router: Router, private authService: AuthService, private infoService: InfoService) { }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      login: new FormControl(``, [Validators.required, Validators.minLength(4)]),
+      email: new FormControl(``, [Validators.required, Validators.minLength(4)]),
       password: new FormControl(``, [Validators.required, Validators.minLength(4)]),
       firstName: new FormControl(``),
       lastName: new FormControl(``),
+      age: new FormControl(``),
       height: new FormControl(``),
       weight: new FormControl(``),
       sex: new FormControl(``),
-      age: new FormControl(``),
     });
   }
 
@@ -45,23 +46,44 @@ export class SignupPage implements OnInit {
     };
 }
 
-public async onSignUp() {
+ async onSignUp() {
+  // if (this.signupForm.invalid) {
+  //   return false;
+  // }
+  //   const login = this.signupForm.get(`email`).value;
+  //   const password = this.signupForm.get(`password`).value;
+  //   const firstName = this.signupForm.get(`firstName`).value;
+  //   const lastName = this.signupForm.get(`lastName`).value;
+  //   const age = this.signupForm.get(`age`).value;
+  //   const  height= this.signupForm.get(`height`).value;
+  //   const weight = this.signupForm.get(`weight`).value;
+  //   const sex = this.signupForm.get(`sex`).value;
+    
+  //   const response = await this.authService.signup(email, password,firstName,lastName,age,height,weight,sex);
+
+
+    
   const response = await this.authService.signup({
       email: this.signupForm.get('email').value,
       password: this.signupForm.get('password').value,
       firstName: this.signupForm.get('firstName').value,
       lastName: this.signupForm.get('lastName').value,
-      avatar: this.avatar,
+      age:this.signupForm.get('age').value,
       height:this.signupForm.get('height').value,
       weight:this.signupForm.get('weight').value,
       sex:this.signupForm.get('sex').value,
-      age:this.signupForm.get('age').value,
+      avatar: this.avatar,
   });
 
   if ((response as any)?.code === API_ERROR_CODES.notUniqueEmail) {
     this.infoService.error(`User with provided email already exists!`);
     return;
 }
+
+// if (response === BPR_ERROR_CODES.internal) {
+//   this.infoService.error(`Cannot connect to the server. Try again later.`);
+//   return;
+// }
 
 if (response?.id) {
     this.infoService.success(`Account has been successfully created!`);
