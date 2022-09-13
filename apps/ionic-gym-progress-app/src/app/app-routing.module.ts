ec4d1fd965a7d9efe5c727278beaa92753a6d565
Kozik Przemysław <przemyk1115@gmail.com>
2022-09-13 22:55:27 +0200

    Added protection guard and guard responsible for redirecting logged users

--- apps/ionic-gym-progress-app/src/app/app-routing.module.ts
+++ apps/ionic-gym-progress-app/src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
     {
         path: '',
+        canActivate: [LoginGuard],
         loadChildren: () => import('./pages/introduction/introduction.module').then((m) => m.IntroductionPageModule),
     },
     {
         path: 'login',
+        canActivate: [LoginGuard],
         loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginPageModule),
     },
     {
         path: 'signup',
+        canActivate: [LoginGuard],
         loadChildren: () => import('./pages/signup/signup.module').then((m) => m.SignupModule),
     },
     {
         path: 'profile/:id',
+        canActivate: [AuthGuard],
         loadChildren: () => import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
     },
 ];
