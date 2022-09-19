f611078b2334608ee829099f019738e152d365c0
Kozik Przemysław <przemyk1115@gmail.com>
2022-09-19 18:11:40 +0200

    Api fix

--- apps/gym-progress-api/src/modules/socket/sessions.module.ts
+++ apps/gym-progress-api/src/modules/socket/sessions.module.ts
@@ -1,19 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SessionsGateway } from './sessions.gateway';
-import { UserSession } from './interfaces';
-import { SubscribeMessage, ConnectedSocket, MessageBody } from '@nestjs/websockets';
-import { Socket } from 'dgram';
 
 @Module({
     imports: [],
     providers: [SessionsGateway],
 })
-export class SessionsModule {
-    private readonly gymWithUsers: Map<string, UserSession[]> = new Map<string, UserSession[]>();
-
-    @SubscribeMessage(`startSession`)
-    async startSession(
-        @ConnectedSocket() socket: Socket,
-        @MessageBody() input: SessionInput,
-    ): Promise<string | Error> {}
-}
+export class SessionsModule {}
