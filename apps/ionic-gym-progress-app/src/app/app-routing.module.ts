7743fe0345127dc217923fc218bad19513c14326
Kozik Przemysław <przemyk1115@gmail.com>
2022-10-01 16:29:22 +0200

    Edit account init

--- apps/ionic-gym-progress-app/src/app/app-routing.module.ts
+++ apps/ionic-gym-progress-app/src/app/app-routing.module.ts
@@ -21,26 +21,33 @@ const routes: Routes = [
         loadChildren: () => import('./pages/signup/signup.module').then((m) => m.SignupModule),
     },
     {
+        path: 'profile/:id/edit',
+        canActivate: [LoginGuard],
+        loadChildren: () => import('./pages/edit-account/edit-account.module').then((m) => m.EditPageModule),
+    },
+    {
         path: 'profile/:id',
         canActivate: [AuthGuard],
         loadChildren: () => import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
     },
-  {
-    path: 'list/list-exercises',
-    loadChildren: () => import('./pages/list/list-exercises/list-exercises.module').then(m => m.ListExercisesPageModule)
-  },
-  {
-    path: 'list/list-machines',
-    loadChildren: () => import('./pages/list/list-machines/list-machines.module').then(m => m.ListMachinesPageModule)
-  },
-  {
-    path: 'list/list-gyms',
-    loadChildren: () => import('./pages/list/list-gyms/list-gyms.module').then(m => m.ListGymsPageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./pages/list/list.module').then( m => m.ListPageModule)
-  },
+    {
+        path: 'list/list-exercises',
+        loadChildren: () =>
+            import('./pages/list/list-exercises/list-exercises.module').then((m) => m.ListExercisesPageModule),
+    },
+    {
+        path: 'list/list-machines',
+        loadChildren: () =>
+            import('./pages/list/list-machines/list-machines.module').then((m) => m.ListMachinesPageModule),
+    },
+    {
+        path: 'list/list-gyms',
+        loadChildren: () => import('./pages/list/list-gyms/list-gyms.module').then((m) => m.ListGymsPageModule),
+    },
+    {
+        path: 'list',
+        loadChildren: () => import('./pages/list/list.module').then((m) => m.ListPageModule),
+    },
 ];
 
 @NgModule({
