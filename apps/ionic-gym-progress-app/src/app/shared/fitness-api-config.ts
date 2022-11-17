7d9b07ca24fcba63fdb55d862ff0c7575602dce9
Estera26 <dumi_estera@yahoo.com>
2022-11-17 15:36:57 +0100

    fitness

--- apps/ionic-gym-progress-app/src/app/shared/fitness-api-config.ts
+++ apps/ionic-gym-progress-app/src/app/shared/fitness-api-config.ts
@@ -6,10 +6,6 @@ export enum API_RESOURCES {
     BURNEDCALORIES = `/burnedcalorie`
 }
 
-export enum API_IMAGE_SIZE {
-    ORIGINAL = `original`,
-    W_500 = `w500`,
-}
 
 export const buildUrl = (resource: API_RESOURCES) => {
     return `${API_URL}${resource}?api_key=${environment.API_KEY}`;
