aa3de4d35234b9575aef5f13dee9148f58d1b207
Kozik Przemysław <przemyk1115@gmail.com>
2022-12-03 14:43:49 +0100

    Fixed monthy tests

--- apps/ionic-gym-progress-app/src/app/components/monthly-stats/monthly-stats.componenet.spec.ts
+++ apps/ionic-gym-progress-app/src/app/components/monthly-stats/monthly-stats.componenet.spec.ts
@@ -6,8 +6,9 @@ import { CommonModule } from '@angular/common';
 import { DateTime } from 'luxon';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MonthlyStatsComponent } from './monthly-stats.component';
 import { Router } from '@angular/router';
-import {MonthlyStatsComponent} from './monthly-stats.component';
+import mockedStats from '../weekly-stats/mocked-stats';
 
 describe('MonthlyStatsComponent', () => {
     let component: MonthlyStatsComponent;
@@ -40,31 +41,25 @@ describe('MonthlyStatsComponent', () => {
     });
 
     it(`should correctly set first and last day of month`, () => {
-
-      // @ts-ignore
-      expect(DateTime.fromJSDate(component.firstMonthDay).toFormat(`dd/MM`)).toEqual(`01/12`);
-
-      // @ts-ignore
-      expect(DateTime.fromJSDate(component.lastMonthDay).toFormat(`dd/MM`)).toEqual(`31/12`);
+        expect(component.firstMonthDay.toFormat(`dd/MM`)).toEqual(`01/12`);
+        expect(component.lastMonthDay.toFormat(`dd/MM`)).toEqual(`31/12`);
     });
 
     it(`should correctly minus month`, () => {
         component.currentDate = DateTime.now();
         component.minusMonth();
-        expect(component.currentDate.toFormat(`dd/MM`)).toEqual(
-            DateTime.now().minus({ months: 1 }).toFormat(`dd/MM`),
-        );
+        expect(component.currentDate.toFormat(`dd/MM`)).toEqual(DateTime.now().minus({ months: 1 }).toFormat(`dd/MM`));
     });
 
     it(`should correctly plus month`, () => {
         component.currentDate = DateTime.now();
         component.plusMonth();
-        expect(component.currentDate.toFormat(`dd/MM`)).toEqual(
-            DateTime.now().plus({ months: 1 }).toFormat(`dd/MM`),
-        );
+        expect(component.currentDate.toFormat(`dd/MM`)).toEqual(DateTime.now().plus({ months: 1 }).toFormat(`dd/MM`));
     });
 
     it(`should correctly calculate stats`, () => {
+        component.trainingSessions = mockedStats;
+
         component.createMonthDataSet();
 
         expect(component.monthStatistics).toEqual({
@@ -76,13 +71,23 @@ describe('MonthlyStatsComponent', () => {
     });
 
     it(`should correctly create training session week map`, () => {
+        component.trainingSessions = mockedStats;
+        const mockDate = new Date(1669748352000);
+        component.updateDaysAndLoadData(DateTime.fromJSDate(mockDate));
         const map = component.createMonthDataSet();
-        expect(Array.from(map.keys()).length).toEqual(7);
 
-        expect(component.lineChartBurnedCalories.config._config.data.datasets[0].data).toEqual([
-            0, 0, 5.38, 59.13, 100, 0, 0,
-        ]);
+        expect(Array.from(map.keys()).length).toEqual(30);
 
-        expect(component.lineChartTimeCalories.config._config.data.datasets[0].data).toEqual([0, 0, 0, 11, 10, 0, 0]);
+        const caloriesResultArray: number[] = Array(30).fill(0);
+        caloriesResultArray[22] = 5.38;
+        caloriesResultArray[23] = 59.13;
+        caloriesResultArray[24] = 100;
+
+        const timeResultArray: number[] = Array(30).fill(0);
+        timeResultArray[23] = 11;
+        timeResultArray[24] = 10;
+
+        expect(component.lineChartBurnedCalories.config._config.data.datasets[0].data).toEqual(caloriesResultArray);
+        expect(component.lineChartTimeCalories.config._config.data.datasets[0].data).toEqual(timeResultArray);
     });
 });
